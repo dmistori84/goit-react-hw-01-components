585b1dd17377a17f79d60154289e7ac31b4afbda
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2022-05-16 16:40:50 +0300

    first

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+import user from './user.json';
+import styles from './Profile.module.css';
+
+export const Profile = () => { 
+    return (
+    <div className={styles.profile}>
+        <div className={styles.description}>
+            <img
+                alt="User avatar"
+                src={user.avatar}
+                className={styles.avatar}
+            />
+            <p className={styles.name}>{user.username}</p>
+            <p className={styles.tag}>@{user.tag}</p>
+            <p className={styles.location}>{user.location}</p>
+        </div>
+
+        <ul className={styles.stats}>
+            <li>
+                <span className={styles.label}>Followers</span>
+                <span className={styles.quantity}>{user.stats.followers}</span>
+            </li>
+            <li>
+                <span className={styles.label}>Views</span>
+                <span className={styles.quantity}>{user.stats.views}</span>
+            </li>
+            <li>
+                <span className={styles.label}>Likes</span>
+                <span className={styles.quantity}>{user.stats.likes}</span>
+            </li>
+        </ul>
+    </div>
+    )
+}
